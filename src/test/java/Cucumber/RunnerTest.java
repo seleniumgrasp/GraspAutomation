6563afa79a192d9b10c6dcc545940c6668b4579a
karthickv <krishnamurthy.karuppusamy@ctc-tcs.com>
2016-05-20 10:56:01 +0530

    test case updated

---
+++ src/test/java/Cucumber/RunnerTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+/**
+ * @author KrishnamurthyK
+ *
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources",
+format = {"pretty",
+		"json:target/cucumber-report1.json",
+		"html:target/test-report1",
+		"junit:target/test-report1.xml"}
+		 
+)
+public class RunnerTest  { 
+
+	
+}
